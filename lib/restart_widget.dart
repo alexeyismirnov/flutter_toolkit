30e465f496c028074f16d301ef5032a20161ca4c
alexeyismirnov <alexey@gmail.com>
2021-11-04 12:42:28 +0800

    added FirebaseAnalytics

--- lib/restart_widget.dart
+++ lib/restart_widget.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
 import 'app_theme.dart';
 
 class RestartWidget extends StatefulWidget {
   static restartApp(BuildContext context) {
     final _RestartWidgetState state =
-    context.findAncestorStateOfType<_RestartWidgetState>();
+        context.findAncestorStateOfType<_RestartWidgetState>();
     state.restartApp();
   }
 
   final Widget content;
-  RestartWidget(this.content);
+  final FirebaseAnalytics analytics;
+
+  RestartWidget(this.content, {this.analytics});
 
   @override
   _RestartWidgetState createState() => _RestartWidgetState();
@@ -38,6 +43,10 @@ class _RestartWidgetState extends State<RestartWidget> {
       locale: context.locale,
       title: "title".tr(),
       home: widget.content,
+      navigatorObservers: [
+        if (widget.analytics != null)
+          FirebaseAnalyticsObserver(analytics: widget.analytics)
+      ],
       theme: AppTheme.getThemeData(context),
       debugShowCheckedModeBanner: false,
     );
