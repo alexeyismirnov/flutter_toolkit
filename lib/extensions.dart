46df2b2ace0030a5be5293bf9c37ca7024ffc1d7
alexeyismirnov <alexey@gmail.com>
2022-04-11 21:17:05 +0800

    isTablet

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -34,4 +34,5 @@ extension Capitalize on String {
 extension ScreenDimensions on BuildContext {
   double get screenWidth => MediaQuery.of(this).size.width;
   double get screenHeight => MediaQuery.of(this).size.height;
+  bool get isTablet => screenWidth > 500;
 }
