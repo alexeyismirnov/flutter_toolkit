da7799f66c72012d4964e0f9e468ec61ab1c64fe
alexeyismirnov <alexey@gmail.com>
2023-06-06 16:11:02 +0800

    cleanup

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 
 extension ExtendedIterable<E> on Iterable<E> {
-  /// Like Iterable<T>.map but callback have index as second argument
-  Iterable<T> mapIndexed<T>(T Function(E e, int i) f) {
-    var i = 0;
-    return map((e) => f(e, i++));
-  }
-
   void forEachIndexed(void Function(E e, int i) f) {
     var i = 0;
     forEach((e) => f(e, i++));
